26f0513587cc69b923af8dd24c78f7817623d231
neha6709 <neha22@sheridancollege.ca>
2023-01-19 13:49:21 -0500

    student details version 1

---
+++ src/printstudentdetails/Student.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package printstudentdetails;
+
+/**
+ * model class - raw data for student
+ * @author dell
+ */
+public class Student 
+{
+    private String name;
+    private int sid ;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
